5395ae1129e405c8f86a0fed46329eade6879756
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-17 11:05:53 +0600

    all datatype

---
+++ datatype.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+void main(){
+  //Data Type
+  //Numbers, String ,Booleans ,Lists ,Maps, Bynamic,
+  //String 
+  String firstName = "rufaida";
+  print("string is : $firstName");
+  //Integers
+  int myNum = 28;
+  print("integer : $myNum");
+  //Doubles
+  double otherNum = 4.5;
+  print(otherNum);
+  //Booleans
+  bool myBool = false;
+  bool mybool = true;
+  print("booleans : $myBool , $mybool");
+  //Dynamic
+}
