157892e18cf4d7ce4f341ce5f07696720a701f0e
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-17 11:26:48 +0600

    list

---
+++ list.dart
@@ -0,0 +1,18 @@
+void main(){
+
+var myList=[1,2,3,4,5,"rima","mina","luna","mira"];
+
+print(myList.length);
+//create an Empty List
+var emptylist = [];
+emptylist.add(85);
+
+print(emptylist);
+emptylist.addAll([2,4,5,6,7,3,5,6]);
+print(emptylist);
+emptylist.insert(2, "who are you");
+print(emptylist);
+emptylist.insertAll(1, [9,6,5,1,7,444,4,7,7777,9856]);
+print(emptylist);
+
+}
