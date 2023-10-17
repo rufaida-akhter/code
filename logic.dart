b2eab43e31392e0257f4f25f76fd17a6b29eca5e
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-17 13:05:48 +0600

    logic if if/else ,if else if,

---
+++ logic.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  //Logic - If , If/ Else, If Else If
+  var num = 11;
+  if(num == 5)
+  {
+    print("the number is 5!");
+  }
+  else if(num == 10)
+  {print("the number is 10;");}
+  else{
+    print("the number is not 5 or 10 it is $num");
+  }
+}
