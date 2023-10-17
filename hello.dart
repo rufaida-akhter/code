1c13184a0e10100fba4adf93733ab0785b98f7fb
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-17 10:25:04 +0600

    variable string integer dynamic const and final null variable

--- hello.dart
+++ hello.dart
@@ -1,3 +1,25 @@
 void main (){
 
-print("start code now it's 17.10.23");}
+print("start code now it's 17.10.23");
+
+//variable in dart
+String name = "akhi";
+print(name);
+//Integer
+var x= 41;
+print(x);
+//Dynamic
+dynamic firstName = "tim";
+print(firstName);
+//Const and final
+const String fullName = "john Elder";
+print(fullName);
+final String nickName = "j";
+print(nickName);
+
+//declearing and not using
+var myName;
+print(myName);
+myName = "rufaida";
+print(myName);
+}
