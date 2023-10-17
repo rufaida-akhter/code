707e65c4994f0fdcb8e2e075f7d4dcd79b445e62
rufaida-akhter <akhter.akhi21@gmail.com>
2023-10-17 12:54:34 +0600

    loops in dart

---
+++ loop.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  //For loop
+  var num = 2;
+  for( var i = num; i >= 1 ; i--)
+  {print(i);}
+  //For IN Loop
+  var names = ["john","tina","rina","sova","lima"];
+  for(var name in names){
+    print(name);
+  }
+ //While Loop
+ while(num <= 10)
+ {print(num);
+ num++;}
+}
